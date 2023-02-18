5c08abe92fbd866d6910858ab5f8a7c11ea2de1d
Viraj76 <virajg.7680@gmail.com>
2023-02-18 23:50:29 +0530

    Retrofit call to the api

--- app/src/main/java/com/example/movies/Retrofit/MovieApi.kt
+++ app/src/main/java/com/example/movies/Retrofit/MovieApi.kt
@@ -1,6 +1,7 @@
 package com.example.movies.Retrofit
 
 import com.example.movies.ModelClasses.AutoComplete
+import com.example.movies.ModelClasses.D
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -9,9 +10,9 @@ import retrofit2.http.Query
 
 interface MovieApi {
     @Headers(
-        "Accept : application/json",
-        "X-RapidAPI-Key  : 322196f639mshb8ac08b332ac693p1c3d66jsn4ef5ad0870a6",
-        "X-RapidAPI-Host : online-movie-database.p.rapidapi.com"
+        "Accept:application/json",
+        "X-RapidAPI-Key:322196f639mshb8ac08b332ac693p1c3d66jsn4ef5ad0870a6",
+        "X-RapidAPI-Host:online-movie-database.p.rapidapi.com"
     )
     @GET("auto-complete?")
     fun getMovies(@Query("q") movieName: String?): Call<AutoComplete?>?
--- app/src/main/java/com/example/movies/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/movies/viewModel/MainViewModel.kt
@@ -1,4 +1,60 @@
 package com.example.movies.viewModel
 
-class MainViewModel {
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.movies.ModelClasses.AutoComplete
+import com.example.movies.ModelClasses.D
+import com.example.movies.Retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel :ViewModel(){
+
+
+    private  var movieDetailLiveData=MutableLiveData<D>()
+
+    fun getMovieDetail(movieName : String) {
+        RetrofitInstance.api.getMovies(movieName)?.enqueue(object : Callback<AutoComplete?>{
+            override fun onResponse(call: Call<AutoComplete?>, response: Response<AutoComplete?>) {
+                val detailList = response.body()?.detailList?.first()
+                detailList?.let {
+                    movieDetailLiveData.postValue(it)
+                }
+            }
+            override fun onFailure(call: Call<AutoComplete?>, t: Throwable) {
+                Log.d("MainActivity",t.message.toString())
+            }
+
+        })
+    }
+
+    fun observeMovieDetailLiveData() :LiveData<D> {
+        return movieDetailLiveData
+    }
+
+
+
+
+
+
+//    fun searchMeals(searchQuery:String){
+//        RetrofitInstance.api.searchMeals(searchQuery).enqueue(object: Callback<MealList> {
+//            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+//                val mealList = response.body()?.meals
+//                mealList?.let {
+//                    searchedMealLiveData.postValue(it)
+//                }
+//            }
+//            override fun onFailure(call: Call<MealList>, t: Throwable) {
+//                var tv: TextView
+//                Log.e("HomeViewModel",t.message.toString())
+//            }
+//
+//        })
+
+
 }
