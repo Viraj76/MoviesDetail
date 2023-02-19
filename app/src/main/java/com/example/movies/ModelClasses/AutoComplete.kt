61536073e5aa92883893a8453697625f4a171446
Viraj76 <virajg.7680@gmail.com>
2023-02-19 22:19:05 +0530

    Edited model classes

--- app/src/main/java/com/example/movies/ModelClasses/AutoComplete.kt
+++ app/src/main/java/com/example/movies/ModelClasses/AutoComplete.kt
@@ -3,8 +3,7 @@ package com.example.movies.ModelClasses
 import com.google.gson.annotations.SerializedName
 
 data class AutoComplete(
-    @SerializedName("d")
-    val detailList: List<D>,
+    val d: List<D>,
     val q: String,
     val v: Int
 )
--- app/src/main/java/com/example/movies/ModelClasses/D.kt
+++ app/src/main/java/com/example/movies/ModelClasses/D.kt
@@ -2,12 +2,12 @@ package com.example.movies.ModelClasses
 
 data class D(
     val i: I,
-    val id: String?,
-    val l: String?,
-    val q: String?,
-    val qid: String?,
-    val rank: Int?,
-    val s: String?,
-    val y: Int?,
-    val yr: String?
+    val id: String,
+    val l: String,
+    val q: String,
+    val qid: String,
+    val rank: Int,
+    val s: String,
+    val y: Int,
+    val yr: String
 )
