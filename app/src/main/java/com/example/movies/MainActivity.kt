7af299c222a82bb4fe27b25330b120e0a730c536
Viraj76 <virajg.7680@gmail.com>
2023-02-22 19:09:11 +0530

    Successfully implemented RV for the all contents of movie list , but app getting crashed for no response of any object

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -6,39 +6,55 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.example.movies.ModelClasses.D
+import com.example.movies.adapters.MovieDetailAdapter
 import com.example.movies.databinding.ActivityMainBinding
 import com.example.movies.viewModel.MainViewModel
+
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(),LifecycleOwner {
     lateinit var binding : ActivityMainBinding
     private lateinit var viewModel : MainViewModel
+    private lateinit var moviesDetailAdapter : MovieDetailAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        moviesDetailAdapter = MovieDetailAdapter()
+        prepareMovieDetailRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 //        viewModel.getMovieDetail()
         binding.ivArrow.setOnClickListener { searchMovie() }
         observeSearchedMovieLiveData()
     }
+
+    private fun prepareMovieDetailRecyclerView() {
+        binding.rvMovieDetails.apply {
+            layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,true)
+            adapter = moviesDetailAdapter
+        }
+    }
+
     private fun observeSearchedMovieLiveData() {
-        viewModel.observeMovieDetailLiveData().observe(this) { movieList ->
-            val size = movieList.size
-            val random = Random.nextInt(0, size - 1)
+        viewModel.observeMovieDetailLiveData().observe(this) { movieDetailList ->
+//            val size = movieList.size
+//            val random = Random.nextInt(0, size - 1
             // here we have used random number to access the data but be careful with displaying the data
 
-            if (movieList != null) {
-                Glide.with(this@MainActivity)
-                    .load(movieList[0].imageOfMovie.imageUrl)   // for this call image is missing then how we can fix it
-                    .into(binding.ivMovieImage)
-                binding.tvMovieName.text = movieList[0].movieName
-                binding.tvRank.text = movieList[0].rankOfMovie.toString()
-                binding.tvStarCast.text = movieList[0].starCastOfMovie
-
-                binding.tvYear.text= movieList[0].yearOfRelease.toString()
-            }
+//            if (movieList != null) {
+//                Glide.with(this@MainActivity)
+//                    .load(movieList[0].imageOfMovie.imageUrl)   // for this call image is missing then how we can fix it
+//                    .into(binding.ivMovieImage)
+//                binding.tvMovieName.text = movieList[0].movieName
+//                binding.tvRank.text = movieList[0].rankOfMovie.toString()
+//                binding.tvStarCast.text = movieList[0].starCastOfMovie
+//                binding.tvType.text = movieList[0].typeId
+//                binding.tvYear.text= movieList[0].yearOfRelease.toString()
+//            }
+            moviesDetailAdapter.setMovieDetail(movieDetailList as ArrayList<D>)
         }
     }
 
--- app/src/main/java/com/example/movies/adapters/MovieDetailAdapter.kt
+++ app/src/main/java/com/example/movies/adapters/MovieDetailAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.movies.adapters
 
-class MovieDetailAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.movies.ModelClasses.D
+import com.example.movies.databinding.MovieDetailItemViewBinding
+import kotlin.time.measureTimedValue
+
+class MovieDetailAdapter() :RecyclerView.Adapter<MovieDetailAdapter.MovieDetailViewHolder>(){
+    private  var movieDetails = ArrayList<D>()
+
+    fun setMovieDetail(movieDetail : ArrayList<D>){
+        this.movieDetails = movieDetail
+        notifyDataSetChanged()
+    }
+    class MovieDetailViewHolder( val binding: MovieDetailItemViewBinding):ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieDetailViewHolder {
+        return MovieDetailViewHolder(MovieDetailItemViewBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MovieDetailViewHolder, position: Int) {
+        if(movieDetails[position] != null){
+            Glide.with(holder.itemView)
+                .load(movieDetails[position].imageOfMovie.imageUrl)
+                .into(holder.binding.ivMovieImage)
+        }
+
+        holder.binding.ivMovieName.text = movieDetails[position].movieName
+        holder.binding.ivMovieStarCast.text = movieDetails[position].starCastOfMovie
+        holder.binding.tvRank.text = movieDetails[position].rankOfMovie.toString()
+        holder.binding.tvType.text = movieDetails[position].typeId
+        holder.binding.tvYearOfRelease.text = movieDetails[position].yearOfRelease.toString()
+    }
+
+    override fun getItemCount(): Int {
+       return movieDetails.size
+    }
+
 }
