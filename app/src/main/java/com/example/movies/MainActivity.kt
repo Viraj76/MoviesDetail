7cceb1cc843f4bf48f7cbc0309e3dacf69f3b576
Viraj76 <virajg.7680@gmail.com>
2023-08-15 19:51:51 +0530

    up to date

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -15,17 +15,21 @@ import com.example.movies.databinding.ActivityMainBinding
 import com.example.movies.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity(),LifecycleOwner {
+
     lateinit var binding : ActivityMainBinding
     private lateinit var viewModel : MainViewModel
     private lateinit var moviesDetailAdapter : MovieDetailAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         moviesDetailAdapter = MovieDetailAdapter()
         prepareMovieDetailRecyclerView()
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-//        viewModel.getMovieDetail()
+
         binding.etSearchBox.requestFocus()
         binding.ivArrow.setOnClickListener {
             searchMovie()
@@ -36,29 +40,10 @@ class MainActivity : AppCompatActivity(),LifecycleOwner {
 
     }
 
-//    private void closeKeyboard()
-//    {
-//        View view = this.getCurrentFocus();
-//        if (view != null) {
-
-//            InputMethodManager manager
-//            = (InputMethodManager)
-//            getSystemService(
-//                Context.INPUT_METHOD_SERVICE);
-//            manager
-//                .hideSoftInputFromWindow(
-//                    view.getWindowToken(), 0);
-//        }
     private fun closeKeyboard() {
-       val view : View? = this.currentFocus
-
-    val manager = getSystemService(
-        Context.INPUT_METHOD_SERVICE
-    ) as InputMethodManager
-    manager
-        .hideSoftInputFromWindow(
-            view!!.windowToken, 0
-        )
+        val view : View? = this.currentFocus
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(view!!.windowToken, 0)
     }
 
     private fun prepareMovieDetailRecyclerView() {
@@ -70,25 +55,9 @@ class MainActivity : AppCompatActivity(),LifecycleOwner {
 
     private fun observeSearchedMovieLiveData() {
         viewModel.observeMovieDetailLiveData().observe(this) { movieDetailList ->
-//            val size = movieList.size
-//            val random = Random.nextInt(0, size - 1
-            // here we have used random number to access the data but be careful with displaying the data
-
-//            if (movieList != null) {
-//                Glide.with(this@MainActivity)
-//                    .load(movieList[0].imageOfMovie.imageUrl)   // for this call image is missing then how we can fix it
-//                    .into(binding.ivMovieImage)
-//                binding.tvMovieName.text = movieList[0].movieName
-//                binding.tvRank.text = movieList[0].rankOfMovie.toString()
-//                binding.tvStarCast.text = movieList[0].starCastOfMovie
-//                binding.tvType.text = movieList[0].typeId
-//                binding.tvYear.text= movieList[0].yearOfRelease.toString()
-//            }
-
             movieDetailList.sortedByDescending { movies->
                 movies.yearOfRelease
             }
-
             moviesDetailAdapter.setMovieDetail(movieDetailList as ArrayList<D>)
         }
     }
@@ -99,6 +68,5 @@ class MainActivity : AppCompatActivity(),LifecycleOwner {
             viewModel.getMovieDetail(searchedMovie)
         }
     }
-
 
 }
--- app/src/main/java/com/example/movies/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/movies/viewModel/MainViewModel.kt
@@ -13,50 +13,27 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel :ViewModel(){
+class MainViewModel :ViewModel() {
 
-    private var movieDetailLiveData=MutableLiveData<List<D>>()
+    private var movieDetailLiveData = MutableLiveData<List<D>>()
 
-    fun getMovieDetail(movieName :String) {
-
-        RetrofitInstance.api.getMovies(movieName)?.enqueue(object : Callback<AutoComplete?>{
+    fun getMovieDetail(movieName: String) {
+        RetrofitInstance.api.getMovies(movieName)?.enqueue(object : Callback<AutoComplete?> {
             override fun onResponse(call: Call<AutoComplete?>, response: Response<AutoComplete?>) {
                 val searchedMovie = response.body()?.d
-
                 searchedMovie?.let {
                     movieDetailLiveData.postValue(it)
                 }
             }
             override fun onFailure(call: Call<AutoComplete?>, t: Throwable) {
-                Log.d("MainActivity",t.message.toString())
+                Log.d("MainActivity", t.message.toString())
             }
 
         })
     }
 
-    fun observeMovieDetailLiveData() :LiveData<List<D>> {
+
+    fun observeMovieDetailLiveData(): LiveData<List<D>> {
         return movieDetailLiveData
     }
-
-
-
-
-
-
-//    fun searchMeals(searchQuery:String){
-//        RetrofitInstance.api.searchMeals(searchQuery).enqueue(object: Callback<MealList> {
-//            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
-//                val mealList = response.body()?.meals
-//                mealList?.let {
-//                    searchedMealLiveData.postValue(it)
-//                }
-//            }
-//            override fun onFailure(call: Call<MealList>, t: Throwable) {
-//                var tv: TextView
-//                Log.e("HomeViewModel",t.message.toString())
-//            }
-//
-//        })
-
-
 }
